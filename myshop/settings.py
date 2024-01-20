2268010e3877b408a67cde97930046cfc4561380
Abbracx <tankoraphael@gmail.com>
2024-01-20 22:07:13 +0100

    core: Added a cart app and functionalities for managing a cart.

--- myshop/settings.py
+++ myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Cart Session
+CART_SESSION_ID = 'cart'
