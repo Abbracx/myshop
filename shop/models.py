9ab1644d5907f2cb64d5c2a6b2dd8bfec22fb76c
Abbracx <tankoraphael@gmail.com>
2024-01-20 02:00:44 +0100

    Added  catalog view

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,6 +12,9 @@ class Category(models.Model):
         ordering = ("name",)
         verbose_name = "category"
         verbose_name_plural = "categories"
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -32,6 +36,9 @@ class Product(models.Model):
     class Meta:
         ordering = ("name",)
         index_together = (("id", "slug"),)
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
 
     def __str__(self):
         return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
 
 # Create your views here.
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(avaialble=True)
+
+    if category_slug:
+        category = get_object_or_404(Category, category_slug)
+        products = products.filter(category=category)
+    return render(
+        request,
+        "shop/product/list.html/",
+        {"categories": categories, "products": products, "category": category},
+    )
+def product_detail(request, id, slug):
+    # Get the get_absolute_url() of a model instance returns the URL that you would
+    # use to reverse a detail view for that object with its primary key.
+    product = get_object_or_404(Product, id=id, slug=slug)  # this is how we can protect against url manipulation
+    return render(request,"shop/product/detail.html",{"product": product})
